36e66e10f2ad106c021650dab11d30530eb23866
galkin1928 <galkin1928@gmail.com>
2024-11-25 17:59:26 +0300

    Initial Commit

---
+++ testVolume/testVolumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testVolumeApp.swift
+//  testVolume
+//
+//  Created by GALKIN Aleksandr on 25.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct testVolumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
